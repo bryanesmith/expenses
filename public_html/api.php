ff4adae34ca61babdb05ac0870e14ddded840991
Bryan Smith <bryanesmith@gmail.com>
2013-06-18 20:37:48 -0400

    Add post support for expenses and categories

--- public_html/api.php
+++ public_html/api.php
@@ -148,6 +148,12 @@
   }
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  function get_json_decoded_request_payload() {
+    $json = file_get_contents('php://input');
+    return json_decode($json, true);
+  }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   function handle_list_json( $sql, $args ) {
     $dbh = get_dbh();
 
@@ -175,6 +181,19 @@
     } else {
       respond_not_found();
     }
+  }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  function handle_post_json( $sql, $args, $resource ) {
+
+    $dbh = get_dbh();
+    $sth = $dbh->prepare( $sql );
+    $sth->execute($args);
+
+    // For response, retrieve newly-created resource
+    $resource['id'] = $dbh->lastInsertId(); 
+    $json = json_encode( $resource ); 
+    respond_json( $json ); 
   }
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -206,33 +225,86 @@
   }
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-  function handle_request( $resource, $method ) { 
+  function handle_post_expense() {
+
+    $expense = get_json_decoded_request_payload(); 
+
+    $sql = "INSERT INTO `expenses`(`date`, `description`, `cost`, `category_id`) VALUES (?, ?, ?, ?)";
+    $args = array( $expense['date'], $expense['description'], $expense['cost'], $expense['category_id'] );
+
+    handle_post_json( $sql, $args, $expense );
+  }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  function handle_post_category() {
+
+    $category = get_json_decoded_request_payload(); 
+
+    $sql = "INSERT INTO `categories`(`category`) VALUES (?)";
+    $args = array( $category['category'] );
+
+    handle_post_json( $sql, $args, $category );
+  }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  function handle_get_request( $resource ) {
     $type = $resource[0];
     $count = count( $resource );
-    switch ( $method ) {
-      case "POST":
-          respond_not_implemented();
-          break;
-      case "GET":
-          if ( $type == 'expenses' ) {
-            if ( $count == 1 ) {
-              handle_list_expenses();
-            } else if ( $count == 2 ) {
-              handle_get_expense( $resource[1] );
-            }
-          } else if ( $type == 'categories' ) {     
-            if ( $count == 1 ) {
-              handle_list_categories();
-            } else if ( $count == 2 ) {
-              handle_get_category( $resource[1] );
-            }
+    switch ( $type ) {
+      case 'expenses':
+          if ( $count == 1 ) {
+            handle_list_expenses();
+          } else if ( $count == 2 ) {
+            handle_get_expense( $resource[1] );
           }
           break;
+      case 'categories':
+          if ( $count == 1 ) {
+            handle_list_categories();
+          } else if ( $count == 2 ) {
+            handle_get_category( $resource[1] );
+          }
+          break;
+    }
+  } // handle_get_request
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  function handle_post_request( $resource ) {
+    $type = $resource[0];
+    switch ( $type ) {
+      case 'expenses':
+          handle_post_expense();
+          break;
+      case 'categories':
+          handle_post_category();
+          break;
+    }
+  } 
+ 
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  function handle_put_request( $resource ) {
+    respond_not_implemented();
+  } 
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  function handle_delete_request( $resource ) {
+    respond_not_implemented();
+  } 
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  function handle_request( $resource, $method ) { 
+    switch ( $method ) {
+      case "POST":
+          handle_post_request( $resource );
+          break;
+      case "GET":
+          handle_get_request( $resource );
+          break;
       case "PUT":
-          respond_not_implemented();
+          handle_put_request( $resource );
           break;
       case "DELETE":
-          respond_not_implemented();
+          handle_delete_request( $resource );
           break;
     }
   }
